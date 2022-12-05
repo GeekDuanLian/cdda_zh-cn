b2bcc2b2279ada265576cf316d5cd2dd1cad8188
GeekDuanLian <dl@pbstu.com>
2022-12-06 04:31:24 +0800

    update check

--- check.py
+++ check.py
@@ -7,18 +7,19 @@ import sys
 old = polib.pofile(sys.argv[1])
 new = polib.pofile(sys.argv[2])
 
-old = {(i.msgid, i.msgctxt): i.msgstr for i in old}
-new = {(i.msgid, i.msgctxt): i.msgstr for i in new}
+old = {(i.msgid, i.msgctxt): i.msgstr_plural[0] if i.msgstr_plural else i.msgstr for i in old}
+new = {(i.msgid, i.msgctxt): i.msgstr_plural[0] if i.msgstr_plural else i.msgstr for i in new}
 
 f = open('fix.html', 'w')
 f.write('<style>table, td { white-space: nowrap; vertical-align: top; border: 1px solid black; border-collapse: collapse; padding: 5px; }</style><table>')
 
 need_fix = False
-def check(s, mark=lambda x: f'<span style="color:red">{x}</span>'):
+def check(s, mark=lambda x: f'<span style="color:red;background-color:yellow">{x}</span>'):
     r = {}
-    if s.endswith(' '): r['多余空格']=''; s = s[:-1]+mark('•')
     for i in ',.;:?!)，。；：？！）\n':
-        if i+' ' in s: r['多余空格']=''; s = s.replace(i+' ', i+mark('•'))
+        if i+' ' in s : r['多余空格']=''; s = s.replace(i+' ', i+mark('•'))
+    if s.endswith(' '): r['多余空格']=''; s = s[:-1]+mark('•')
+    if '\xa0' in s: r['非法空格']=''; s = s.replace('\xa0', mark(r'\xa0'))
     if '..' in s: r['英文省略号']=''; s = re.sub('([.]{2,})', mark(r'\1'), s)
     #if any([i in s for i in '‘’“”']): r['中文引号']=''; s = re.sub('([‘’“”])', mark(r'\1'), s)
     if r:
@@ -27,7 +28,7 @@ def check(s, mark=lambda x: f'<span style="color:red">{x}</span>'):
 
 for k, v in new.items():
     if k in old and old[k] == v: continue # same
-    check(v)
+    if v: check(v)
 
 f.write('</table>'); f.close()
 if not need_fix: import os; os.remove(f.name)
