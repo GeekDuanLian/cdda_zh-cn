2a35c3b7cc88cdd11b840a65769700ca2514c7a1
GeekDuanLian <dl@pbstu.com>
2022-12-19 00:32:14 +0800

    添加检测 重复符号

--- check.py
+++ check.py
@@ -15,20 +15,24 @@ f.write('<style>table, td { white-space: nowrap; vertical-align: top; border: 1p
 
 need_fix = False
 def check(s, mark=lambda x: f'<span>{x}</span>'):
-    r = {}; s = s.replace('<', '&lt;')
-    for i in ',.;:?!()<>[]"&，。；：？！（）《》【】“”\n':
+    r = {}; s = s.replace('<', '\x00')
+    en = ',.;:?!()<>[]'
+    zh = '，。；：？！（）《》【】'
+    enq = '\'"'
+    zhq = '‘’“”'
+    sym = '{}@#$%^&*-_=+|`~\\/\n\x00'
+    for i in en+zh+enq+zhq+sym:
         if i+' ' in s : r['多余空格']=''; s = s.replace(i+' ', i+mark(' '))
         if ' '+i in s : r['多余空格']=''; s = s.replace(' '+i, mark(' ')+i)
+        if i not in '\n?!？！' and (i:=i+i) in s: r['重复符号']=''; s = s.replace(i, mark(i))
+    for a, b in zip(en+''.join([i*2 for i in enq])+'.', zh+zhq+'…'):
+        if (i:=a+b) in s: r['重复符号']=''; s = s.replace(i, mark(i))
+        if (i:=b+a) in s: r['重复符号']=''; s = s.replace(i, mark(i))
     if s.  endswith(' '): r['多余空格']=''; s = s[:-1]+mark(' ')
     if s.startswith(' '): r['多余空格']=''; s = mark(' ')+s[1:]
     if '\xa0' in s: r['非法空格']=''; s = s.replace('\xa0', mark(r'\xa0'))
-    if '..' in s: r['英文省略号']=''; s = re.sub('([.]{2,})', mark(r'\1'), s)
-    if '.…' in s: r['混用省略号']=''; s = s.replace('.…', mark('.…'))
-    if '….' in s: r['混用省略号']=''; s = s.replace('….', mark('….'))
-    #if any([i in s for i in '‘’“”']): r['中文引号']=''; s = re.sub('([‘’“”])', mark(r'\1'), s)
-    if r:
-        f.write('<tr><td>'+'<br>'.join(r.keys())+'</td><td><pre>'+s.replace('\n', '<br>')+'</pre></td></tr>')
-        global need_fix; need_fix = True
+    #if any([i in s for i in zhq]): r['中文引号']=''; s = re.sub(f'([{zhq}])', mark(r'\1'), s)
+    if r: f.write('<tr><td>'+'<br>'.join(r.keys())+'</td><td><pre>'+s.replace('\x00', '&lt;').replace('\n', '<br>')+'</pre></td></tr>'); global need_fix; need_fix = True
 
 for k, v in new.items():
     if k in old and old[k] == v: continue # same
